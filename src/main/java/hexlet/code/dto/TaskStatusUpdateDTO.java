07b4ab7e8f78c939023e12a6f444fb327ff8f190
DSunShine371 <pestic077@gmail.com>
2025-10-07 14:22:55 +0300

    Step 10.9.6 fix: Status for tests

--- src/main/java/hexlet/code/dto/TaskStatusUpdateDTO.java
+++ src/main/java/hexlet/code/dto/TaskStatusUpdateDTO.java
@@ -1,14 +1,19 @@
 package hexlet.code.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.openapitools.jackson.nullable.JsonNullable;
 
 @Data
 public class TaskStatusUpdateDTO {
-    private JsonNullable<@Size(min = 1, message = "Name must contain at least 1 character.")
+    private JsonNullable<
+            @NotBlank(message = "Name cannot be empty.")
+            @Size(min = 1, message = "Name must contain at least 1 character.")
             String> name;
 
-    private JsonNullable<@Size(min = 1, message = "Slug must contain at least 1 character.")
+    private JsonNullable<
+            @NotBlank(message = "Slug cannot be empty.")
+            @Size(min = 1, message = "Slug must contain at least 1 character.")
             String> slug;
 }
